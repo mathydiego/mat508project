14043afabc41bb7442fc47f2a940ca13ed548d2e
Diego Avalos <30484284+mathydiego@users.noreply.github.com>
2018-02-18 18:46:39 -0800

    Update mat508project.m

--- mat508project.m
+++ mat508project.m
@@ -1,6 +1,6 @@
 format long
 rng('shuffle')
-n=4; k = 10^n;
+n=0; k = 10^n;
 r =  k*randi([-5,5],1,10);
 S = [ 0     r(1) r(2)  r(3)   r(4);...
      -r(1)   0   r(5)  r(6)   r(7);...
@@ -11,13 +11,14 @@ MatlabExponential = expm(S)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Computing my exponential
 e = imag(eig(S)); e = e(e>0);
-t1 = min(abs(e));
-t2 = max(abs(e));
+t1 = min(abs(e))/k;
+t2 = max(abs(e))/k;
+S = (1/k)*S;
 
-c1 = (t2^3*sin(t1)-t1^3*sin(t2))/(t1*t2*(t2^2-t1^2));
-c2 = (t2^4*(1-cos(t1))-t1^4*(1-cos(t2)))/(t1^2*t2^2*(t2^2-t1^2));
-c3 = (t2*sin(t1)-t1*sin(t2))/(t1*t2*(t2^2-t1^2));
-c4 = (t2^2*(1-cos(t1))-t1^2*(1-cos(t2)))/(t1^2*t2^2*(t2^2-t1^2));
+c1 = (t2^3*sin(k*t1)-t1^3*sin(k*t2))/(t1*t2*(t2^2-t1^2));
+c2 = (t2^4*(1-cos(k*t1))-t1^4*(1-cos(k*t2)))/(t1^2*t2^2*(t2^2-t1^2));
+c3 = (t2*sin(k*t1)-t1*sin(k*t2))/(t1*t2*(t2^2-t1^2));
+c4 = (t2^2*(1-cos(k*t1))-t1^2*(1-cos(k*t2)))/(t1^2*t2^2*(t2^2-t1^2));
 
 MyExponential = eye(5) + c1*S + c2*S^2 + c3*S^3 + c4*S^4
 rel_error = MyExponential-MatlabExponential;
