dce9a6f11ffd20e9eb4ea6622ee7b26da53c7821
Diego Avalos <30484284+mathydiego@users.noreply.github.com>
2018-03-07 11:07:33 -0800

    Update mat508project.m

--- mat508project.m
+++ mat508project.m
@@ -40,3 +40,43 @@ for j = 1:100
 plot(0:20,0.01*log(errors),'o');
 end
 %saveas(gca, 'mat508plot.eps','eps');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%determinant evaluation%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+close all
+N=100;
+dets = zeros(1,N);
+Dets = zeros(1,N);
+
+for j = 1:N
+    r=randi([-10 10],1,10);
+    [M m Er] = expskew5(r,0);
+    dets(j)=abs(1-det(m));
+    Dets(j)=abs(1-det(M));
+end
+plot(1:N, dets, 'b',1:N, Dets,'r');
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% expskew5 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [MyExponential, MatlabExponential, rel_error] = expskew5(r,k)
+% r is a 1x10 vector, the terms correspond to the entries above the
+% diagonal of the skew-symmetric matrix S.
+format long
+S = [ 0     r(1) r(2)  r(3)   r(4);...
+     -r(1)   0   r(5)  r(6)   r(7);...
+     -r(2) -r(5)  0    r(8)   r(9);...
+     -r(3) -r(6) -r(8) 0      r(10);...
+     -r(4) -r(7) -r(9) -r(10)  0    ];
+k=10^k;
+MatlabExponential = expm(k*S);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Computing my exponential
+A = charpoly(S); a = A(3); b = A(5);
+t1 = sqrt((a-sqrt(a^2-4*b))/2);
+t2 = sqrt((a+sqrt(a^2-4*b))/2);
+
+c1 = (t2^3*sin(k*t1)-t1^3*sin(k*t2))/(t1*t2*(t2^2-t1^2));
+c2 = (t2^4*(1-cos(k*t1))-t1^4*(1-cos(k*t2)))/(t1^2*t2^2*(t2^2-t1^2));
+c3 = (t2*sin(k*t1)-t1*sin(k*t2))/(t1*t2*(t2^2-t1^2));
+c4 = (t2^2*(1-cos(k*t1))-t1^2*(1-cos(k*t2)))/(t1^2*t2^2*(t2^2-t1^2));
+
+MyExponential = eye(5) + c1*S + c2*S^2 + c3*S^3 + c4*S^4;
+E = MyExponential-MatlabExponential;
+rel_error = norm(E);
